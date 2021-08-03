527158bf73e4729d8ada224b44e8156a227241cd
djamper103 <xodorenko95@gmail.com>
2021-08-03 17:57:19 +0300

    first commit

--- src/components/Sidebar/Messeges/People/People.jsx
+++ src/components/Sidebar/Messeges/People/People.jsx
@@ -5,7 +5,7 @@ const People =(props)=>{
     return(
         <div className={style.people}>
               <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"></img>
-                <NavLink src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" to={'/People/'+props.id}> {props.name} </NavLink>
+                <NavLink src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" to={'/People/'+props.id}> <span>{props.name} </span></NavLink>
         </div>
     )
 }
