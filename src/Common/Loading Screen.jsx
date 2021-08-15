99de4a9026528cf382b27bd6fb87e825e1cdabdd
djamper103 <xodorenko95@gmail.com>
2021-08-15 14:09:47 +0300

    first commit

--- src/Common/Loading Screen.jsx
+++ src/Common/Loading Screen.jsx
@@ -1,26 +1,23 @@
 import React, {useState,useEffect} from "react"
 import CircleLoader from "react-spinners/ClipLoader";
-
-
+import style from "./Loader.module.css"
 
 const LoadingScreen = () => {
 
     let [loading,setLoading]=useState(false)
-
 
     useEffect(()=>{
         setLoading(true)
     },[])
 
     return (
-        <div>
+        <div className={style.loader}>
             {
                 loading ?
-                    <CircleLoader loading={loading} color={"red"} size={500} />
+                    <CircleLoader loading={loading} color={"red"} size={300} />
                     :null
             }
         </div>
-
     )
 }
 export default LoadingScreen;
